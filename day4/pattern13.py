ea11435d8295fb764026be10cc4caf7db46e499f
Manvi Gupta <manvi.official1212@gmail.com>
2026-08-09 18:43:55 +0530

    Add continuous number triangle pattern

---
+++ day4/pattern13.py
@@ -0,0 +1,13 @@
+# 1
+# 2 3
+# 4 5 6
+# 7 8 9 10
+# 11 12 13 14 15
+num = 0
+
+for i in range (0,5):
+    
+    for j in range (0,i+1):
+        num = num+1
+        print ( num , end = " ")
+    print ()
